7caf703726cb299e6a8d862fe46fac2f39a09157
saurabh290 <saurabh2937kumar@gmail.com>
2025-09-14 23:08:10 +0530

    const added

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let x = 4;
+    let CONSTANT_VALUE: u32 = 100_000; // constant value
+
+    println!("constant value is: {CONSTANT_VALUE}");
+
     println!("value of x is: {x}");
 
     {
