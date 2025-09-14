bfc90f57b0b1955e1eb2613f630e65e244167458
saurabh290 <saurabh2937kumar@gmail.com>
2025-09-14 23:05:27 +0530

    variable: COMMIT_EDITMSG

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
     let x = 4;
     println!("value of x is: {x}");
+
+    {
+        let x = x * 2;
+        println!("value of x in inner scope is: {x}");
+    }
+
+    let x = "hello";
+    println!("new value of x is: {x}");
 }
